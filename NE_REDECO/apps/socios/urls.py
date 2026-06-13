7ac32197e6e4d5dbd56f787adbad8c35e013b33a
Eduardo Picazo <tavio.jtx@gmail.com>
2026-06-13 16:42:08 -0600

    listo crud socios

--- NE_REDECO/apps/socios/urls.py
+++ NE_REDECO/apps/socios/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.lista_socios, name='lista_socios'),
+    path('buscar-cp/', views.buscar_cp, name='buscar_cp'),
+    path('agregar/', views.agregar_socio, name='agregar_socio'),
 ]
